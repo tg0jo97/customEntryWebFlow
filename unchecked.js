c146de522d580f146229caab4f61603643defa92
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-11-16 22:42:45 +0100

    Add files via upload

---
+++ unchecked.js
@@ -0,0 +1,9 @@
+function DisplayBtn() {
+  var selectCheckedBox = document.getElementById("divCheckbox");
+  if (selectCheckedBox.value == "on") {
+    document.getElementById("buttonNextT").style.display = "block";
+  } else {
+    document.getElementById("buttonNextT").style.display = "none";
+  }
+}
+DisplayBtn();
