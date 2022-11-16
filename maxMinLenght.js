5db1031ef1b0e94687c27333fc3179c046ac258c
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-11-16 21:29:10 +0100

    Delete maxMinLenght.js

--- maxMinLenght.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("codePostale").setAttribute("minlength", 5);
-document.getElementById("codePostale").setAttribute("maxlength", 5);
-document.getElementById("codePostale").setAttribute("value", "yaRien");
