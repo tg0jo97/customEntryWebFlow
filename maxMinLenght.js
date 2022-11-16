20d30ec3d2decc3972e580c5ab788ebd11060b65
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-11-16 18:39:06 +0100

    Add files via upload

---
+++ maxMinLenght.js
@@ -0,0 +1,3 @@
+document.getElementById("codePostale").setAttribute("minlength", 5);
+document.getElementById("codePostale").setAttribute("maxlength", 5);
+document.getElementById("codePostale").setAttribute("value", "yaRien");
