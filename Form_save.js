e1eaddee91459113845eb56f841f47c252db2a30
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-12-03 21:28:42 +0100

    Create Form_save.js

---
+++ Form_save.js
@@ -0,0 +1,7 @@
+
+Webflow.push(function() {
+  $('#email-form').submit(function() {
+    setTimeout(function() {
+      location.href = 'https://calendly.com/huggydev?hide_landing_page_details=1&hide_gdpr_banner=1?&email=' + $('#Email').val() + '&full_name=' + $('#Nom').val(); }, 100);
+  });
+});
