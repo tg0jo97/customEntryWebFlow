ade65f1b9c628a51d482d61d3b106e640a00902b
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-12-10 12:06:45 +0100

    Update piegeInput.js
    Last Update

--- piegeInput.js
+++ piegeInput.js
@@ -78,21 +78,21 @@ function validateCodePostale(str) {
 }
 function validateNom(str) {
   // check length, if is a number, if is whole number, if no periods
-  return /^[A-Z][A-Za-z\é\è\ê\-]+$/.test(str);
+  return /^[a-zA-Z ]+$/.test(str);
 }
 function validatePrenom(str) {
   // check length, if is a number, if is whole number, if no periods
-  return /^[A-Z][A-Za-z\é\è\ê\-]+$/.test(str);
+  return /^[a-zA-Z ]+$/.test(str);
 }
 function validateEmail(str) {
   // check length, if is a number, if is whole number, if no periods
-  return /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$/.test(
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
     str
   );
 }
 function validatePhone(str) {
   // check length, if is a number, if is whole number, if no periods
-  return /^((\+)33|0)[1-9](\d{2}){4}$/.test(
+  return /^[0-9]{0,5}$/.test(
     str
   );
 }
