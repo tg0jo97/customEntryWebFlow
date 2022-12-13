8bfd6d0e08e21816e1ff6a964df1b96e55541594
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-12-13 15:56:06 +0100

    Delete checked.js

--- checked.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("divCheckbox");
-if (selectCheckedBox.value == "on") {
-  document.getElementById("buttonNextT").style.display = "block";
-} else {
-  document.getElementById("buttonNextT").style.display = "none";
-}
