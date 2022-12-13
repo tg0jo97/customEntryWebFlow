970f2a3c3ac9c8e7ebc8187ac64fd3b7ab7ca810
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-12-13 15:59:02 +0100

    Create DisplayBareme.js

---
+++ DisplayBareme.js
@@ -0,0 +1,93 @@
+<p><span id='display14'></span></p>
+<script async type="text/javascript">
+//affiche 1 Personne dans le foyer 
+document.getElementById("Inferieur15262").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="1personnes"]:checked')?.value;});
+document.getElementById("Entre15262et19565").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="1personnes"]:checked')?.value;});
+document.getElementById("Entre19565et29148").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="1personnes"]:checked')?.value;});
+document.getElementById("Superieur29148").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="1personnes"]:checked')?.value;});
+//affiche 2 Personne dans le foyer 
+document.getElementById("Inferieur22320").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="2personnes"]:checked')?.value;});
+document.getElementById("Entre22320et28614").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="2personnes"]:checked')?.value;});
+document.getElementById("Entre28614et42848").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="2personnes"]:checked')?.value;});
+document.getElementById("Superieur42848").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="2personnes"]:checked')?.value;});
+//affiche 3 Personne dans le foyer 
+document.getElementById("Inferieur26844").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="3personnes"]:checked')?.value;});
+document.getElementById("Entre26844et34441").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="3personnes"]:checked')?.value;});
+document.getElementById("Entre34411et51592").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="3personnes"]:checked')?.value;});
+document.getElementById("Superieur51592").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="3personnes"]:checked')?.value;});
+//affiche 4 Personne dans le foyer 
+document.getElementById("Inferieur31359").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="4personnes"]:checked')?.value;});
+document.getElementById("Entre31359et40201").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="4personnes"]:checked')?.value;});
+document.getElementById("Entre40201et60336").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="4personnes"]:checked')?.value;});
+document.getElementById("Superieur60336").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="4personnes"]:checked')?.value;});
+//affiche 5 Personne dans le foyer 
+document.getElementById("Inferieur35894").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="5personnes"]:checked')?.value;});
+document.getElementById("Entre35894et46015").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="5personnes"]:checked')?.value;});
+document.getElementById("Entre46015et69081").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="5personnes"]:checked')?.value;});
+document.getElementById("Superieur69081").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="5personnes"]:checked')?.value;});
+//affiche 6 Personne dans le foyer 
+document.getElementById("Inferieur40420").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="6personnes"]:checked')?.value;});
+document.getElementById("Entre40420et51812").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="6personnes"]:checked')?.value;});
+document.getElementById("Entre51812et77825").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="6personnes"]:checked')?.value;});
+document.getElementById("Superieur77825").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="6personnes"]:checked')?.value;});
+//affiche 7 Personne dans le foyer 
+document.getElementById("Inferieur44946").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="7personnes"]:checked')?.value;});
+document.getElementById("Entre44946et57609").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="7personnes"]:checked')?.value;});
+document.getElementById("Entre57609et86569").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="7personnes"]:checked')?.value;});
+document.getElementById("Superieur86569").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="7personnes"]:checked')?.value;});
+//affiche 8 Personne dans le foyer 
+document.getElementById("Inferieur49472").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="8personnes"]:checked')?.value;});
+document.getElementById("Entre49472et63406").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="8personnes"]:checked')?.value;});
+document.getElementById("Entre63406et95313").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="8personnes"]:checked')?.value;});
+document.getElementById("Superieur95313").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="8personnes"]:checked')?.value;});
+//affiche 9 Personne dans le foyer 
+document.getElementById("Inferieur53998").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="9personnes"]:checked')?.value;});
+document.getElementById("Entre26844et34441").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="9personnes"]:checked')?.value;});
+document.getElementById("Entre69203et104057").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="9personnes"]:checked')?.value;});
+document.getElementById("Superieur104057").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="9personnes"]:checked')?.value;});
+//affiche 10 Personne dans le foyer 
+document.getElementById("Inferieur58524").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="10personnes"]:checked')?.value;});
+document.getElementById("Entre58524et75000").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="10personnes"]:checked')?.value;});
+document.getElementById("Entre75000et112801").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="10personnes"]:checked')?.value;});
+document.getElementById("Superieur112801").addEventListener('click',function(){
+document.getElementById('display14').innerHTML = document.querySelector('input[name="10personnes"]:checked')?.value;});
+</script>
