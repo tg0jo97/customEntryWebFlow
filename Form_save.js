d6b1d93e5ac2ac48a86aca89b2012f28ba4ec882
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-12-13 23:17:13 +0100

    Update Form_save.js

--- Form_save.js
+++ Form_save.js
@@ -1,6 +1,6 @@
 Webflow.push(function() {
     $('#email-form').submit(function() {
       setTimeout(function() {
-        location.href = 'https://calendly.com/huggydev?hide_landing_page_details=1&hide_gdpr_banner=1?&email=' + $('#Email').val() + '&full_name=' + $('#Nom').val() + '&=question_0' + $('#UUID').val(); }, 100);
+        location.href = 'https://calendly.com/huggydev?hide_landing_page_details=1&hide_gdpr_banner=1?&email=' + $('#Email').val() + '&full_name=' + $('#Nom').val() + '&question_0' + $('#UUID').val(); }, 100);
     });
   });
