6547faffc995020e1955277b95f57a34b578ee8b
tg0jo97 <104874583+tg0jo97@users.noreply.github.com>
2022-12-15 22:43:18 +0100

    Create rfr.js

---
+++ rfr.js
@@ -0,0 +1,292 @@
+//function Masque All ,only value 1 is display              V
+if(document.querySelector("#pvf").value == '1 Personne')
+{
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Intallation RfR impossible');
+}
+//Select 2                       V
+function Select2(){
+if(document.querySelector("#pvf").value == '2 Personnes')
+{
+//element afficher uniquement  2
+document.querySelector("#RfRdiv2").style.display = 'inline-flex';
+//element masquer tous sauf 2
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 2 Personnes');
+}
+}
+//Select 3                       V
+function Select3(){
+if(document.querySelector("#pvf").value == '3 Personnes')
+{
+//element afficher uniquement  3
+document.querySelector("#RfRdiv3").style.display = 'inline-flex';
+//element masquer tous sauf 3
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 3 Personnes');
+}
+}
+//Select 4                       V
+function Select4(){
+if(document.querySelector("#pvf").value == '4 Personnes')
+{
+//element afficher uniquement  4
+document.querySelector("#RfRdiv4").style.display = 'inline-flex';
+//element masquer tous sauf 4
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 4 Personnes');
+}
+}
+//Select 5                       V
+function Select5(){
+if(document.querySelector("#pvf").value == '5 Personnes')
+{
+//element afficher uniquement  5
+document.querySelector("#RfRdiv5").style.display = 'inline-flex';
+//element masquer tous sauf 5
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 5 Personnes');
+}
+}
+//Select 6                       V
+function Select6(){
+if(document.querySelector("#pvf").value == '6 Personnes')
+{
+//element afficher uniquement  6
+document.querySelector("#RfRdiv6").style.display = 'inline-flex';
+//element masquer tous sauf 6
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 6 Personnes');
+}
+}
+//Select 7                       V
+function Select7(){
+if(document.querySelector("#pvf").value == '7 Personnes')
+{
+//element afficher uniquement  7
+document.querySelector("#RfRdiv7").style.display = 'inline-flex';
+//element masquer tous sauf 7
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 7 Personnes');
+}
+}
+//Select 8                       V
+function Select8(){
+if(document.querySelector("#pvf").value == '8 Personnes')
+{
+//element afficher uniquement  8
+document.querySelector("#RfRdiv8").style.display = 'inline-flex';
+//element masquer tous sauf 8
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant8 Personnes');
+}
+}
+//Select 9                       V
+function Select9(){
+if(document.querySelector("#pvf").value == '9 Personnes')
+{
+//element afficher uniquement  9
+document.querySelector("#RfRdiv9").style.display = 'inline-flex';
+//element masquer tous sauf 9
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 9 Personnes');
+}
+}
+//Select 10                       V
+function Select10(){
+if(document.querySelector("#pvf").value == '10 Personnes')
+{
+//element afficher uniquement  10
+document.querySelector("#RfRdiv10").style.display = 'inline-flex';
+//element masquer tous sauf 10
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 10 Personnes');
+}
+}
+function Select11(){
+if(document.querySelector("#pvf").value == '11 Personnes')
+{
+//element afficher uniquement  11
+document.querySelector("#RfRdiv11").style.display = 'inline-flex';
+//element masquer tous sauf 11
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 11 Personnes');
+}
+}
+function Select12(){
+if(document.querySelector("#pvf").value == '12 Personnes')
+{
+//element afficher uniquement  12
+document.querySelector("#RfRdiv12").style.display = 'inline-flex';
+//element masquer tous sauf 12
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv1").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 12 Personnes');
+}
+}
+//Function 1
+function Select1(){
+if(document.querySelector("#pvf").value == '1 Personne')
+{
+//element afficher uniquement  12
+document.querySelector("#RfRdiv1").style.display = 'inline-flex';
+//element masquer tous sauf 12
+document.querySelector("#RfRdiv2").style.display = 'none';
+document.querySelector("#RfRdiv12").style.display = 'none';
+document.querySelector("#RfRdiv3").style.display = 'none';
+document.querySelector("#RfRdiv4").style.display = 'none';
+document.querySelector("#RfRdiv5").style.display = 'none';
+document.querySelector("#RfRdiv6").style.display = 'none';
+document.querySelector("#RfRdiv7").style.display = 'none';
+document.querySelector("#RfRdiv8").style.display = 'none';
+document.querySelector("#RfRdiv9").style.display = 'none';
+document.querySelector("#RfRdiv10").style.display = 'none';
+document.querySelector("#RfRdiv11").style.display = 'none';
+}else{
+console.log('Pas de famille ayant 0');
+}
+}
+//Lancemennt des functions par déclencheur des buttons id
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select1();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select2();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select3();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select4();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select5();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select6();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select7();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select8();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select9();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select10();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select11();},1000));
+document.getElementById("NextRfR").addEventListener('click',() => setInterval(function(){Select12();},1000));
